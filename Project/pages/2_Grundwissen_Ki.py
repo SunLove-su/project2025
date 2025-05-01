3bfe1d8860358a2cc98e229069e9a8e66cf29c2c
sunlove <techDoku89@outlook.de>
2025-05-01 17:32:39 +0200

    2_Grundwissen_kibegriffe

--- Project/pages/2_Grundwissen_Ki.py
+++ Project/pages/2_Grundwissen_Ki.py
@@ -11,36 +11,65 @@ st.markdown("""
             """)
 with st.expander("Was ist KI"):
      st.markdown("""
-                    Stellt euch ein Labyrinth mit vielen Wegen und Sackgassen vor.
+                    Stellt euch ein Labyrinth mit Sackgassen und mit vielen unterschiedlichen Wegen vor,
+                    bei dem nur einer führt zum Ausgang.
+                 
+                    Vor der KI musste ich einem Computerprogramm genaue Angaben geben Gehe zwei Schritte
+                    nach vorne, einen nach links, drei nach vorne. Gibt es Hindernisse wie eine Mauer dann
+                    bleibt das Programm stehen, da es dazu keine Angeben erhalten hat. Beim Programmieren
+                    muss jede Möglichkeit, jedes Hinderniss und jeder Schritt beschrieben sein,
+                    ansonsten bleibt das Programm an der Stelle mit dem Hinterniss stehen.
+                 
+                    Mit KI ist es anders. Die KI geht 1.000.000 in unterschiedliche Labyrinthe und merkt sich,
+                    wie ein Hindernis aussieht, dass sie nicht weiter kann und merkt sich den besten Weg zum
+                    Ausgang.Anhand von Mustern hat die KI gelernt, dass wenn links eine Wand ist, sie rechts
+                    weiter gehen kann.oder das bei 3 Wegen der mittlere meistens zum Ziel geführt hat. Bei
+                    Labyrithen die die KI noch nie gegangen ist, kann es passieren dass sie sich verläuft,
+                    den falschen Weg geht und in einer Sackgasse endet
 
-                    Vor der KI musste ich einem Computerprogramm exakte Anweisungen
-                    geben: "Gehe einen Schritte nach vorne, dann zwei nach links, dann
-                    einen nach rechts." Trifft es auf eine Mauer, bleibt das Programm stehen,
-                    weil es keine Anweisung für diesen Fall hat. Jede Situation muss vorprogrammiert sein.
-
-                    KI funktioniert anders: Sie geht durch tausende Labyrinthe und lernt daraus.
-                    Sie merkt sich Muster: "Bei einer Wand links gehe rechts" oder "Bei drei Wegen
-                    führt meist der mittlere zum Ziel". Trifft die KI auf ein völlig unbekanntes Labyrinth,
-                    kann sie dennoch in Sackgassen enden.
-
-                    **Der Unterschied: Wir Menschen können schon beim ersten Labyrinth intuitiv Probleme lösen. KI braucht Millionen Übungen, um unsere Fähigkeit zum Kombinieren und Entscheiden nachzuahmen.""", unsafe_allow_html=True
+                    Während Du und ich beim ersten Mal in einem Labyrinth im Gegensatz zur KI wissen, wie wir
+                    aus einer Sackgasse kommen und uns den Ausgang ersuchen, braucht eien KI 1.000.0000 Übungen.
+                    Mit diesen Übungen versucht sie unsere Intelligenz, die Kombinationen, Schlussfolgerungen
+                    und Entscheidungen die wir treffen nachzuahmen."""
      )
 
 with st.expander("Wie funktioniert KI"):
-     st.write("Wir haben mit dem Labyrinth gesehen, dass die KI:" \
-               "1. viele Daten braucht: 1.000.000 ist die KI die unterschiedlichen Labyrithen durchgegangen"
-               "2. sich Muster merkt: wenn links eine Wand ist, sie rechts weiter geht und das bei 3 Wegen der mittlere meistens ins Ziel führt" \
-               "3. erlerntes anwendet: die KI begeht ein Labyrinth, die sie noch nie gegangen ist und gelangt an den Ausgang" \
-               "4. fehler macht: die KI verläuft sich, geht den falschen Weg und steckt in einer Sackgasse fest ")
+     st.markdown("""
+                 Wir haben mit dem Labyrinth gesehen, dass die KI:
+                 1. viele Daten braucht: 1.000.000 ist die KI die unterschiedlichen Labyrithen durchgegangen
+                 2. sich Muster merkt: wenn links eine Wand ist, sie rechts weiter geht und das bei 3 Wegen der
+                    mittlere meistens ins Ziel führt 
+                 3. erlerntes anwendet und daraus vorhersagen trifft: die KI begeht ein Labyrinth, die sie noch nie gegangen ist und gelangt
+                    an den Ausgang
+                 4. fehler macht: die KI verläuft sich, geht den falschen Weg und steckt in einer Sackgasse fest 
+                """)
+
 
 with st.expander("KI-Begriffe"):
-     st.write( " - Algorithmus: Schritt für Schritt Anleitung, wie das Computer-Programm, dem ich sagen musste gehe zwei Schritte nach vorne, einen nach links."
-               " - Machine Learning: "
-               "2. sich Muster merkt: wenn links eine Wand ist, sie rechts weiter geht und das bei 3 Wegen der mittlere meistens ins Ziel führt" \
+     st.markdown("""
+                - Algorithmus: Schritt für Schritt Anleitung, wie das Computer-Programm, dem ich sagen musste gehe zwei Schritte nach vorne, einen nach links."
+                - Machine Learning: Ist die KI, die 1.000.000 mal durch die Labyrithen muss, sich die Muster und Merkmale merkt"
+                   - Überwachtes Lernen: Ich begleite die KI bei einigen Labyrinthen und zeige ihr den richtigen Weg.
+                   - Unüberwachtes Lernen: Die geht die 1.000.000 mal alleine durch die Labyrinthe und merkt sich die Muster
+                   - Künstliche Neuronale Netze (KNN): Sollen den Aufbau und die Funktionsweise eines Gehirns nachahmen.
+                                                       Beispiel Fußballspiel: Der linke Torwart schießt den Ball, einem freien Verteidiger,
+                                                       dieser zum Mittelfeldspieler, dann zum Stürmer. Jeder Spieler entscheidet zu wem er den
+                                                       Ball spielt.
+                                                       Die Spieler stellen Neuronen dar, die den Ball also die Informationen erhalten, verarbeiten und weitergeben.
+                                                       Als Gesamtbild stellt es ein Netz dar. 
+                                                       Durch das Trainig wird ihr zusammenspiel besser, sodass sie in der Lage sind Titel zu holen oder
+                                                       bei der KI komplexe Aufgaben zu lösen
+                   - Deep Learning: Ist ein vielschichtigeres Netz, mit mehreren Ebenen.
+                                    Beispiel es sind jetzt viel mehr Spieler zwischen den Positionen auf dem Feld, z. B. zwischen dem Torwart und dem
+                                    Mittelspieler
+                                    Die einzelnen Spieler in den 
+                 
+                 
+                                                      
                "3. erlerntes anwendet: die KI begeht ein Labyrinth, die sie noch nie gegangen ist und gelangt an den Ausgang" \
                "4. fehler macht: die KI verläuft sich, geht den falschen Weg und steckt in einer Sackgasse fest ")
 
-
+                """)
 with st.expander("Was kann KI"):
      st.write("")
 

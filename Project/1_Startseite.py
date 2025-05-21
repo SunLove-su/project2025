370ede79f2756765c1689b4ac617e369d82e0020
sunlove <techDoku89@outlook.de>
2025-05-21 13:50:48 +0200

    Kommentare Startseite

--- Project/1_Startseite.py
+++ Project/1_Startseite.py
@@ -9,27 +9,33 @@ hilfsdatei.seite("Lerneinheit zur Sensibilisierung von KI-generierten Inhalten")
 #Anmeldung mit Passwort, weil bei StreamlitCloud jeder Zugriff hätte
 hilfsdatei.login()
 
+#Überschrift des Schulmoduls
 st.markdown("<h4>Willkommen zum Lerneinheit zur Sensibilisierung KI-generierter Inhalte</h4>", unsafe_allow_html=True)
+
+#Einleitung des Schulmoduls
 st.markdown("""Diese Lerneinheit soll die Risiken von KI-generierten Inhalten aufzeigen und dich für diese sensibilisieren.
 Künstliche Intelligenz (KI) findet heute überall Einsatz und bietet uns viele Chancen.
 Es gibt aber auch einige Risiken beim Umgang mit KI, z. B. das die generierten Informationen falsch sind u.a. Diese werden in der Lerneinheit exemplarisch aufgezeigt,
 damit du bewusster mit KI-Anwendungen umgehen kannst!""")
 
+#Trennungslinie
 st.divider()
 
+#Aufbau des Schulmoduls
 st.markdown("""
-                    ***Dich erwarten:***
-                      - ***Einstieg***: Beantworte Fragen zu deinem Nutzungsverhalten und denem Vertrauen zu KI-generierten Inhalten.
-                      - ***Grundwissen***: Lerne etwas über die KI und die Anwendungsbereiche.
-                      - ***Übungen***: Erkenne KI-generierte Inhalte und interagiere mit KI-Anwendungen.
-                      - ***Abschluss***: Beantworte erneut Fragen und reflektiere das Gelernte.
+                ***Dich erwarten:***
+                  - ***Einstieg***: Beantworte Fragen zu deinem Nutzungsverhalten und denem Vertrauen zu KI-generierten Inhalten.
+                  - ***Grundwissen***: Lerne etwas über die KI und die Anwendungsbereiche.
+                  - ***Übungen***: Erkenne KI-generierte Inhalte und interagiere mit KI-Anwendungen.
+                  - ***Abschluss***: Beantworte erneut Fragen und reflektiere das Gelernte.
             """)
 
-#Grafische Trennung durch eine Linie       
+#Trennungslinie       
 st.divider()
-#Inhalt über das Modul
-st.markdown("***Hinweise zur Lerneinheit:***")
+
+#Inhalt des Moduls
 st.markdown("""
+                ***Hinweise zur Lerneinheit:***
                 - Dauer: ca. 40 - 45 Minuten
                 - Teilnahme: anonym
                 - Freiwilligkeit: freiwillige Teilnahme
@@ -37,17 +43,25 @@ st.markdown("""
                   Es wird sichergestellt, dass eine Zurückverfolgung auf die teilnehmende Person nicht erfolgt.
                   Die Daten werden ausschließlich für mein Forschungsprojekt verwendet.
             """)
-#Grafische Trennung durch eine Linie
+
+#Trennungslinie
 st.divider()
 
+#Text zum Start des Schulmoduls
 st.markdown("Bist du Bereit mit der Lerneinheit zu beginnen und über KI zu lernen, dann klicke auf \"Start\" ")
+
+
 #Anpassung der Position des Buttons, CSS funktioniert hier nicht nur durch Hijacking.
 #Anpassung durch Spalten, wobei die erste 80% und die andere 20 % annimmt. 
 #Bei entsprechender Auflösung und Gerät ist der Button rechts. Bei kleineren Seiten funktioniert das nicht
 #Spalten werden bei kleinen Bildschirmen immer untereinander dargestellt.
 
+#2 Spalten erstellen
 col1, col2 = st.columns([8,2])
+
+#In der 2 Spalte also rechts soll der Button plaziert werden
 with col2:
+  
 #Abfrage zur nächsten Seite.
     if st.button("Start"):
 #Beginn der Messung der Startzeit der Umfrage.

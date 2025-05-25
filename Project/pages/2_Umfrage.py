ee93779438b4ea790e2e913775c5daaeea9bba12
sunlove <techDoku89@outlook.de>
2025-05-25 18:48:55 +0200

    Anpassung Variablennamen Umfrage

--- Project/pages/2_Umfrage.py
+++ Project/pages/2_Umfrage.py
@@ -1,34 +1,48 @@
+"""
+Einstiegsumfragen (Pre-Tests)
+Erfragung von KI-Kenntnissen, Nutzung usw.
+"""
+
 import streamlit as st
 import hilfsdatei
  
 #Überschrift der Seite
-hilfsdatei.seite("Einstiegsumfrage")
+titel_seite = "Einstiegsumfrage"
+hilfsdatei.seite(titel_seite)
 #Alle Seiten mit PW versehen, dass weiterhin die Teilnehmer die Navigation nicht sehen
 hilfsdatei.login()
 
+ueberschrift_seite = "Einstiegsumfrage"
 #Überschrift der Sektion
-st.markdown("<h4>Einstiegsumfrage</h4>",unsafe_allow_html=True)
+st.markdown(f"<h4>{ueberschrift_seite}</h4>",unsafe_allow_html=True)
 
 #Einleitung der Sektion
-st.markdown("""
-            Die Umfrage erfasst deine persönliche Erfahrung und Einschätzung
-            von KI.
-            Die Antworten werden nur im Rahmen dieser Arbeit ausgewertet.           
+einleitung_text = (
+            """
+                Die Umfrage erfasst deine persönliche Erfahrung und Einschätzung
+                von KI.
+                Die Antworten werden nur im Rahmen dieser Arbeit ausgewertet.           
            """)
+st.markdown(einleitung_text)
 #Trennungslinie
 st.divider()
 #Speicherung der Antworten in "einstiegsumfrage"
-#Dictonary das die Antworten speichert, damit sie später im Abschluss an die Datenbank übergeben werden kann
-if "einstiegsumfrage" not in st.session_state:
-    st.session_state.einstiegsumfrage ={}
+#Dictonary, das die Antworten speichert, damit sie später im Abschluss an die Datenbank übergeben werden kann
+
+speichern_einstiegsumfrage = "einstiegsumfrage"
+if speichern_einstiegsumfrage not in st.session_state:
+    st.session_state[speichern_einstiegsumfrage] ={}
+
+#Alternativ:
+#st.session_state.einstiegsumfrage = {}
 
 
 # Demografische Daten
 
 #Frage nach dem Alter der Teilnehmenden
 #Alter der Jugendlichen in den Studien Vodafone(2024) = 14-20, Sinus (2024) = 14-17
-fragealter = "Wie alt bist du?"
-alter = st.radio (fragealter,
+frage_alter = "Wie alt bist du?"
+antwort_alter = st.radio (frage_alter,
                                 (
                                     "Unter 15 Jahre",
                                     "15 - 16 Jahre",
@@ -38,25 +52,25 @@ alter = st.radio (fragealter,
                                     "Keine Angabe"
     
                                 ),
-# index= None, damit nicht schon eine "Vorauswahl" besteht.
+                                # index= None, damit nicht schon eine "Vorauswahl" besteht.
                                 index=None
 )
 #Speicherung der Antwort
-if alter is not None:
+if antwort_alter is not None:
     st.session_state.einstiegsumfrage["alter"]={
     "Bereich": "Einstiegsumfrage",
     "Typ": "Alter",
-    "Frage":   fragealter,
-    "Antwort": alter
+    "Frage":   frage_alter,
+    "Antwort": antwort_alter
     }
-    st.markdown(f"Deine Antwort: {alter}.")
+    st.markdown(f"Deine Antwort: {antwort_alter}.")
 
 
    
 
 # Frage Geschlecht:
-fragegeschlecht = "Welchem Geschlecht fühlst du dich zugehörig?"
-geschlecht = st.radio(fragegeschlecht,
+frage_geschlecht = "Welchem Geschlecht fühlst du dich zugehörig?"
+antwort_geschlecht = st.radio(frage_geschlecht,
                                   ("Weiblich",
                                    "Männlich",
                                    "Divers",
@@ -65,24 +79,24 @@ geschlecht = st.radio(fragegeschlecht,
                                    index=None
 )
 #Speicherung der Antwort
-if geschlecht is not None:
+if antwort_geschlecht is not None:
  st.session_state.einstiegsumfrage["geschlecht"]={
     "Bereich":"Einstiegsumfrage",
     "Typ":"Geschlecht",
-    "Frage": fragegeschlecht,
-    "Antwort": geschlecht
+    "Frage": frage_geschlecht,
+    "Antwort": antwort_geschlecht
  
  }
 
- st.markdown(f"Deine Antwort: {geschlecht}.")
+ st.markdown(f"Deine Antwort: {antwort_geschlecht}.")
 #Trennungslinie
 st.divider()
 ##############
 
 # Frage KI-Wissen (Selbsteinschätzung)
-frage_kiwissen = "Wie gut kennst du dich mit Künstlicher Intelligenz (KI) aus?"
-kiwissen = st.radio(
-                    frage_kiwissen,
+frage_ki_wissen = "Wie gut kennst du dich mit Künstlicher Intelligenz (KI) aus?"
+antwort_ki_wissen = st.radio(
+                    frage_ki_wissen,
                     ("Sehr gut",
                      "Gut",
                      "Mittel",
@@ -93,48 +107,47 @@ kiwissen = st.radio(
 )
 
 #Speicherung der Antwort
-if kiwissen is not None:
-    st.session_state.einstiegsumfrage["kiwissen"]={
+if antwort_ki_wissen is not None:
+    st.session_state.einstiegsumfrage["ki_wissen"]={
         "Bereich":"Einstiegsumfrage",
         "Typ": "KI-Wissen",
-        "Frage": frage_kiwissen,
-        "Antwort": kiwissen
+        "Frage": frage_ki_wissen,
+        "Antwort": antwort_ki_wissen
     
     }
-    st.markdown (f"Deine Antwort: {kiwissen}.")
+    st.markdown (f"Deine Antwort: {antwort_ki_wissen}.")
 
 #Frage: Erkennungsfähigkeit, ob ein Text oder Bild von der KI generiert wurde
 
-frage_erkennungsfaehigkeit = "Wie gut kannst du erkennen, ob ein Text oder Bild von einer KI stammt?"
-erkennungsfaehigkeit = st.radio(
-    frage_erkennungsfaehigkeit,
-    [
-        "Sehr gut",
-        "Gut", 
-        "Mittel",
-        "Eher schlecht",
-        "Schlecht",
-        "Keine Angabe"
-    ],
-    index=None
-)
+frage_erkennung_ki = "Wie gut kannst du erkennen, ob ein Text oder Bild von einer KI stammt?"
+antwort_erkennung_ki = st.radio(frage_erkennung_ki,
+                        (
+                            "Sehr gut",
+                            "Gut", 
+                            "Mittel",
+                            "Eher schlecht",
+                            "Schlecht",
+                            "Keine Angabe"
+                        ),
+                        index=None
+                    )
 #Speicherung der Antwort
-if erkennungsfaehigkeit is not None:
-    st.session_state.einstiegsumfrage["erkennungsfaehigkeit"] = {
+if antwort_erkennung_ki is not None:
+    st.session_state.einstiegsumfrage["erkennung_ki"] = {
         "Bereich": "Einstiegsumfrage",
         "Typ": "Erkennungsfähigkeit",
-        "Frage": frage_erkennungsfaehigkeit,
-        "Antwort": erkennungsfaehigkeit
+        "Frage": frage_erkennung_ki,
+        "Antwort": antwort_erkennung_ki
     }
-    st.markdown(f"Deine Antwort: {erkennungsfaehigkeit}.")
+    st.markdown(f"Deine Antwort: {antwort_erkennung_ki}.")
 
 #########################
 
 # Nutzunghäufigkeit (Vodafone2024) S. 11 , Gerlich Studie 2025
 #Frage, wie oft KI-Anwendungen genutzt werden
-fragehaeufigkeitkinutzung = "Wie oft nutzt du KI-Anwendungen (z. B. ChatGPT, DALL·E, Perplexity usw.)?"
-haeufigkeitkinutzung = st.radio(
-                                fragehaeufigkeitkinutzung,
+frage_nutzung_ki = "Wie oft nutzt du KI-Anwendungen (z. B. ChatGPT, DALL·E, Perplexity usw.)?"
+antwort_nutzung_ki = st.radio(
+                                frage_nutzung_ki,
                                 ("Täglich",
                                 "Mehrmals die Woche",
                                 "Einmal pro Woche",
@@ -145,23 +158,23 @@ haeufigkeitkinutzung = st.radio(
                                  index=None,
 )
 #Speicherung der Antwort
-if haeufigkeitkinutzung is not None:
-    st.session_state.einstiegsumfrage["haeufigkeitsnutzung"]={
+if antwort_nutzung_ki is not None:
+    st.session_state.einstiegsumfrage["nutzung_ki"]={
     "Bereich": "Einstiegsumfrage",
     "Typ": "Häufigkeitsnutzung",
-    "Frage": fragehaeufigkeitkinutzung,
-    "Antwort": haeufigkeitkinutzung,
+    "Frage": frage_nutzung_ki,
+    "Antwort": antwort_nutzung_ki,
     
     }
-    st.markdown(f"Deine Antwort: {haeufigkeitkinutzung}")
+    st.markdown(f"Deine Antwort: {antwort_nutzung_ki}")
    
 #Trennungslinie
 st.divider()
 
 #Frage für wie sehr KI Inhalten vertraut wird
-fragevertrauenkiinhalte = "Für wie vertrauenswürdig hältst du KI-generierte Inhalte?"
-vertrauenkiinhalten = st.radio(
-                               fragevertrauenkiinhalte,
+frage_vertrauen_ki = "Für wie vertrauenswürdig hältst du KI-generierte Inhalte?"
+antwort_vertrauen_ki = st.radio(
+                                 frage_vertrauen_ki,
                                 ("Sehr vertrauenswürdig",
                                 "Eher vertrauenswürdig",
                                 "Neutral",
@@ -171,24 +184,22 @@ vertrauenkiinhalten = st.radio(
                                index=None
 )
 #Speicherung der Antwort
-if vertrauenkiinhalten is not None:
-
-
-    st.session_state.einstiegsumfrage["vertrauenkiinhalten"]={
+if antwort_vertrauen_ki is not None:
+    st.session_state.einstiegsumfrage["vertrauen_ki"]={
 
     "Bereich":"Einstiegsumfrage",
     "Typ": "Vertrauen KI Inhalte",
-    "Frage": fragevertrauenkiinhalte,
-    "Antwort": vertrauenkiinhalten,
+    "Frage": frage_vertrauen_ki,
+    "Antwort": antwort_vertrauen_ki ,
     }
-    st.markdown(f"Deine Antwort: {vertrauenkiinhalten}")
+    st.markdown(f"Deine Antwort: {antwort_vertrauen_ki}")
 
 
 #Frage ob KI-generierte Inhalte geprüft werden
 
-fragepruefungvorher = "Wie genau prüfst du KI-generierte Inhalte, bevor du ihnen vertraust?"
-pruefungvorher = st.radio(
-                fragepruefungvorher,
+frage_pruefung_ki = "Wie genau prüfst du KI-generierte Inhalte, bevor du ihnen vertraust?"
+antwort_pruefung_ki = st.radio(
+                frage_pruefung_ki,
                 ("Sehr genau - ich prüfe alles, bis ins kleinste Detail",
                  "Eher genau - ich prüfe, aber nicht alles bis ins kleinste Detail",
                  "Manchmal - ich prüfe je nach Thema",
@@ -198,15 +209,15 @@ pruefungvorher = st.radio(
                 index=None
 )
 #Speicherung der Antwort
-if pruefungvorher is not None:
-    st.session_state.einstiegsumfrage["pruefungvorher"]={
+if antwort_pruefung_ki is not None:
+    st.session_state.einstiegsumfrage["pruefung_ki"]={
         "Bereich": "Einstiegsumfrage",
         "Typ": "Prüfung KI",
-        "Frage": fragepruefungvorher,
-        "Antwort": pruefungvorher
+        "Frage": frage_pruefung_ki,
+        "Antwort": antwort_pruefung_ki
     
     }
-    st.markdown(f"Deine Antwort: {pruefungvorher}")
+    st.markdown(f"Deine Antwort: {antwort_pruefung_ki}")
     
 
 
@@ -228,25 +239,25 @@ st.progress (1/8)
 if st.button("Weiter"):
     unbeantwortet = False
 #Prüfung, ob alle Eingaben erfolgt sind
-    if alter is None:
+    if antwort_alter is None:
         st.error("Bitte gib dein Alter an.")
         unbeantwortet = True
-    if geschlecht is None:
+    if antwort_geschlecht is None:
         st.error("Bitte gib dein Geschlecht an.")
         unbeantwortet = True
-    if kiwissen is None:
+    if antwort_ki_wissen is None:
         st.error("Bitte gib deinen Wissensstand an.")
         unbeantwortet = True
-    if erkennungsfaehigkeit is None:
+    if antwort_erkennung_ki is None:
         st.error ("Bitte gib deine Einschätzung zur Erkennung von KI-generierten Inhalten an.")
         unbeantwortet = True
-    if haeufigkeitkinutzung is None:
+    if antwort_nutzung_ki is None:
         st.error ("Bitte gib an, wie häufig du KI nutzt.")
         unbeantwortet = True
-    if vertrauenkiinhalten is None:
+    if antwort_vertrauen_ki is None:
         st.error ("Bitte gib an, wie sehr du KI-generierten Inhalten vertraust")
         unbeantwortet = True
-    if pruefungvorher is None:
+    if antwort_pruefung_ki is None:
         st.error ("Bitte gib an, ob du KI prüfst.")
         unbeantwortet = True
 #Wenn alle Pflichtfelder beantwortet sind, dann kann der Teilnehmer auf die nächste Seite

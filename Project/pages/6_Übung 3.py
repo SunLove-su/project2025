8b24b165cffbab58baf8977f0cef495fc1b96a85
sunlove <techDoku89@outlook.de>
2025-05-25 20:49:38 +0200

    variablennamen Anpassung Übung 3

--- Project/pages/6_Übung 3.py
+++ Project/pages/6_Übung 3.py
@@ -19,40 +19,43 @@ except KeyError:
     st.error("Kein API Key für OpenAI vorhanden. Abfragen über OpenAI nicht möglich")
 
 #Überschrift der Seite
-hilfsdatei.seite("3. Übung")
+titel_seite = "3. Übung"
+hilfsdatei.seite(titel_seite)
 #Sicherstellen, dass ein Zugriff der Seiten nur mit Passwort erfolgt, und dass User keine Navigationsseite sehen
 hilfsdatei.login()   
 
 #Überschrift auf der Seite
-st.markdown("<h4>3. Übung</h4>",unsafe_allow_html=True)
+ueberschrift_seite = "3. Übung"
+st.markdown(f"<h4>{ueberschrift_seite}</h4>",unsafe_allow_html=True)
 
 #Aufklärung über das vorherige Bild
-st.markdown("""
+einleitung_text =("""
             In der Übung zuvor hast du die Person gesehen, die das Eis isst.
             Bei dem Bild handelt es sich um ein KI-generiertes Bild von der Seite "thispersondoesnotexist.com", das sehr echt aussieht.
             
             Für KI-generierte Bilder wird der Deep Learning Algorithmus Generative Adversarial Networks (GAN) verwendet.
             """)
+st.markdown(einleitung_text)
 
-fragebildreal="Wie realistisch fandest du das Bild von der vorherigen Übung?"
-bildreal=st.radio(fragebildreal,
-                                  ["Sehr realisitisch",
+frage_bild_realistisch="Wie realistisch fandest du das Bild von der vorherigen Übung?"
+antwort_bild_realistisch=st.radio(frage_bild_realistisch,
+                                  ("Sehr realisitisch",
                                    "Eher realisitisch",
                                    "Mittelmäßig",
                                    "Eher unrealistisch",
                                    "Sehr unrealisitisch",
-                                   "Keine Angabe"
-                                   ], index=None
+                                   "Keine Angabe"),
+                                    index=None
                         )
-if bildreal is not None:
+if antwort_bild_realistisch is not None:
         
-        st.session_state.uebung3["bildreal"]={
+        st.session_state.uebung3["bild_realistisch"]={
             "Bereich":"Übung3",
-            "Typ": "KI Bild Real",
-            "Frage": fragebildreal,
-            "Antwort": bildreal
+            "Typ": "KI Bild Realistisch",
+            "Frage": frage_bild_realistisch,
+            "Antwort": antwort_bild_realistisch
         }
-        st.write("Deine Antwort ist:",bildreal)
+        st.write(f"Deine Antwort ist: {antwort_bild_realistisch}.")
 
 
 #Aufgabenstellung für die Teilnehmer
@@ -85,13 +88,13 @@ st.divider()
 if "uebung3" not in st.session_state:
     st.session_state.uebung3 ={}
 
-if "anzahl_bildgenerierungen" not in st.session_state:
-    st.session_state.anzahl_bildgenerierungen = 0
+if "zaehler_bildgenerierung" not in st.session_state:
+    st.session_state.zaehler_bildgenerierung = 0
 
 
 #Container um den Fokus zu behalten
-containerfokus = st.container()
-with containerfokus:
+container_fokus = st.container()
+with container_fokus:
     with st.expander("Bildgenerierung", expanded=True):    
         # Eingabe und Button
         with st.form("frage_formular4", clear_on_submit=True):
@@ -115,8 +118,8 @@ with containerfokus:
                     with st.spinner(text="Generiere Bild, bitte warten..."):
                     
                         #Zählen wie oft der Teilnehmer Bilder generiert
-                        st.session_state.anzahl_bildgenerierungen += 1
-                        aktuelle_anzahl = st.session_state.anzahl_bildgenerierungen
+                        st.session_state.zaehler_bildgenerierung += 1
+                        aktuelle_anzahl = st.session_state.zaehler_bildgenerierung
                         
                         #DALL-E API Aufruf
                         antwort = client.images.generate(
@@ -134,17 +137,17 @@ with containerfokus:
                         st.image(generiertesBild, width=200)
                         
                         #Speichern der Daten
-                        if "BildgenerierenKI" not in st.session_state.uebung3:
-                            st.session_state.uebung3["BildgenerierenKI"] = []
+                        if "bild_generierung_ki" not in st.session_state.uebung3:
+                            st.session_state.uebung3["bild_generierung_ki"] = []
                         
-                        st.session_state.uebung3["BildgenerierenKI"].append({
+                        st.session_state.uebung3["bild_generierung_ki"].append({
                             "Bereich": "Übung3",
                             "Typ": "DALL-E Bilderstellung",
                             "Frage": "Bitte beschreibe, wie dein Bild generiert werden soll",
                             "Antwort": eingabe,
                             "Anzahl Bildgenerierungen": aktuelle_anzahl
                         })
-                        st.session_state.uebung3["BildgenerierenKI"]
+                        st.session_state.uebung3["bild_generierung_ki"]
 
                 except Exception as error:
                     hilfsdatei.openai_fehlerbehandlung(error)
@@ -155,37 +158,47 @@ with containerfokus:
 
                     """)
 #Speichern der Prompts:
-fragedatenschutz = "Würdest du von dir ein Bild generieren lassen, indem du ein Bild von dir hochlädst?"
-datenschutz=st.radio(fragedatenschutz,
-    ["Ja, ich würde ein Bild von mir hochladen",
+frage_datenschutz = "Würdest du von dir ein Bild generieren lassen, indem du ein Bild von dir hochlädst?"
+antwort_datenschutz=st.radio(frage_datenschutz,
+    ("Ja, ich würde ein Bild von mir hochladen",
      "Neutral",
      "Nein, ich würde kein Bild von mir hochladen",
      "Keine Angabe"
-    ], index=None
+    ), index=None
     )
  #Ausgabe der Antwort
-if datenschutz is not None:
+if antwort_datenschutz is not None:
     
     st.session_state.uebung3["datenschutz"] = {
         "Bereich":"Übung3",
         "Typ" : "Datenschutz",
-        "Frage" : fragedatenschutz,
-        "Antwort": datenschutz
+        "Frage" : frage_datenschutz,
+        "Antwort": antwort_datenschutz
     }
-    st.write("Deine Antwort ist:",datenschutz)
+    st.markdown(f"Deine Antwort ist: {antwort_datenschutz}")
 
-frageurheberrecht="Findest du es in Ordnung, dass Bilder im Stil von bekannten Firmen und Künstlern innerhalb von Minuten generiert werden, obwohl diese Jahre lang daran arbeiten?"
-urheberrecht=st.radio(frageurheberrecht,
-                                  ["Ja, ich finde es in Ordnung",
+frage_urheberrecht="Findest du es in Ordnung, dass Bilder im Stil von bekannten Firmen und Künstlern innerhalb von Minuten generiert werden, obwohl diese Jahre lang daran arbeiten?"
+antwort_urheberrecht=st.radio(frage_urheberrecht,
+                                  ("Ja, ich finde es in Ordnung",
                                    "Neutral",
                                    "Nein, ich finde es nicht in Ordnung",
                                    "Keine Angabe"
-                                   ], index=None
+                                  ), index=None
                         )
+if antwort_urheberrecht is not None:
+        
+        st.session_state.uebung3["urheberrecht"]={
+            "Bereich":"Übung3",
+            "Typ": "Urheberrecht",
+            "Frage": frage_urheberrecht,
+            "Antwort": antwort_urheberrecht
+        }
+        st.markdown(f"Deine Antwort ist: {antwort_urheberrecht}")
+
 
 
 frage_unterscheidung = "Wie einfach oder schwierig ist es deiner Meinung nach, KI-generierte Bilder von echten zu unterscheiden?"
-unterscheidung = st.radio(frage_unterscheidung,
+antwort_unterscheidung = st.radio(frage_unterscheidung,
                                 ["Sehr einfach",
                                 "Eher einfach",
                                 "Teils/teils", 
@@ -195,23 +208,16 @@ unterscheidung = st.radio(frage_unterscheidung,
                                 ], index=None, key="unterscheidung"
                             )
     
-if unterscheidung is not None:
+if antwort_unterscheidung is not None:
     st.session_state.uebung3["unterscheidung"] = {
         "Bereich": "Übung3", 
         "Typ": "Bewertung",
         "Frage": frage_unterscheidung,
-        "Antwort": unterscheidung
+        "Antwort": antwort_unterscheidung
     }
+    st.markdown(f"Deine Antwort ist: {antwort_unterscheidung}")
 
-if urheberrecht is not None:
-        
-        st.session_state.uebung3["urheberrecht"]={
-            "Bereich":"Übung3",
-            "Typ": "Urheberrecht",
-            "Frage": frageurheberrecht,
-            "Antwort": urheberrecht
-        }
-        st.write("Deine Antwort ist:",urheberrecht)
+
     
 st.session_state.uebung3
 st.divider()
@@ -221,21 +227,22 @@ st.markdown("Aktueller Fortschritt in der gesamten Lerneinheit: 5 von 8")
 st.progress (5/8)
 if st.button("Weiter"):
     unbeantwortet = False
-    if bildreal is None:
-        st.error("Bitte beantworte die Frage mit dem Bild real.")
+    if antwort_bild_realistisch is None:
+        st.error("Bitte beantworte die Frage, ob das Bild realistisch wirkt.")
         unbeantwortet = True
-    if datenschutz is None:
+    if antwort_datenschutz is None:
         st.error("Bitte beantworte die Frage mit dem Datenschutz.")
         unbeantwortet = True
-    if unterscheidung is None:
+    if antwort_unterscheidung is None:
         st.error("Bitte beantworte die Frage zur Unterscheidung von KI-Bildern.")
         unbeantwortet = True    
-    if urheberrecht is None:
+    if antwort_urheberrecht is None:
         st.error("Bitte beantworte die Frage mit dem Urheberrecht.")
         unbeantwortet = True
-    if "BildgenerierenKI" not in st.session_state.uebung3:
+    if "bild_generierung_ki" not in st.session_state.uebung3:
         st.error("Bitte lass von der KI ein Bild generieren.")
         unbeantwortet = True
     
     if not unbeantwortet:
-        st.switch_page("pages/7_Übung 4.py")
+        naechste_seite=("pages/7_Übung 4.py")
+        st.switch_page(naechste_seite)

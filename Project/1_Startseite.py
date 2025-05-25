0754f2a60fc94f31bb4acfdcb5e177f4aa490ffc
sunlove <techDoku89@outlook.de>
2025-05-25 18:07:26 +0200

    Anpassung Startseite

--- Project/1_Startseite.py
+++ Project/1_Startseite.py
@@ -1,3 +1,10 @@
+"""
+Startseite des Schulmoduls zur Sensibilisierung KI-generierter Inhalte
+Beschreibung des Schulmoduls und Hinweise
+"""
+
+# Importieren der notwendigen Module
+
 import streamlit as st
 #datetime zur Erfassung der Startzeit bei der Umfrage
 import datetime
@@ -5,68 +12,71 @@ import datetime
 import hilfsdatei
 
 #Überschrift der Seite
-hilfsdatei.seite("Lerneinheit zur Sensibilisierung von KI-generierten Inhalten")
+titel_seite = "Lerneinheit zur Sensibilisierung von KI-generierten Inhalten"
+hilfsdatei.seite(titel_seite)
 #Anmeldung mit Passwort, weil bei StreamlitCloud jeder Zugriff hätte
 hilfsdatei.login()
 
 #Überschrift des Schulmoduls
-st.markdown("<h4>Willkommen zum Lerneinheit zur Sensibilisierung KI-generierter Inhalte</h4>", unsafe_allow_html=True)
+ueberschrift_seite = "Willkommen zur Lerneinheit zur Sensibilisierung KI-generierter Inhalte"
+st.markdown(f"<h4>{ueberschrift_seite}</h4>", unsafe_allow_html=True)
 
 #Einleitung des Schulmoduls
-st.markdown("""Diese Lerneinheit soll die Risiken von KI-generierten Inhalten aufzeigen und dich für diese sensibilisieren.
-Künstliche Intelligenz (KI) findet heute überall Einsatz und bietet uns viele Chancen.
-Es gibt aber auch einige Risiken beim Umgang mit KI, z. B. das die generierten Informationen falsch sind u.a. Diese werden in der Lerneinheit exemplarisch aufgezeigt,
-damit du bewusster mit KI-Anwendungen umgehen kannst!""")
+einleitung_text ="""
+                  Diese Lerneinheit soll die Risiken von KI-generierten Inhalten aufzeigen und
+                  dich für diese sensibilisieren. Künstliche Intelligenz (KI) findet heute überall
+                  Einsatz und bietet uns viele Chancen. Es gibtaber auch einige Risiken beim Umgang
+                  mit KI, z. B. dass die generierten Informationen falsch sind u.a. Diese werden in
+                  der Lerneinheit exemplarisch aufgezeigt, damit du bewusster mit KI-Anwendungen
+                  umgehen kannst!"""
+st.markdown(einleitung_text)
 
 #Trennungslinie
 st.divider()
 
 #Aufbau des Schulmoduls
-st.markdown("""
-                ***Dich erwarten:***
+aufbau_schulmodul="""
+                  ***Dich erwarten:***
                   - ***Einstieg***: Beantworte Fragen zu deinem Nutzungsverhalten und denem Vertrauen zu KI-generierten Inhalten.
                   - ***Grundwissen***: Lerne etwas über die KI und die Anwendungsbereiche.
                   - ***Übungen***: Erkenne KI-generierte Inhalte und interagiere mit KI-Anwendungen.
                   - ***Abschluss***: Beantworte erneut Fragen und reflektiere das Gelernte.
-            """)
+            """
+st.markdown(aufbau_schulmodul)
 
 #Trennungslinie       
 st.divider()
 
-#Inhalt des Moduls
-st.markdown("""
+#Hinweise zum Modul
+hinweise_modul=(
+            """
                 ***Hinweise zur Lerneinheit:***
                 - Dauer: ca. 40 - 45 Minuten
                 - Teilnahme: anonym
                 - Freiwilligkeit: freiwillige Teilnahme
                 - Datenschutz: Demografische Daten (Alter, Geschlecht) werden vertraulich behandelt.
-                  Es wird sichergestellt, dass eine Zurückverfolgung auf die teilnehmende Person nicht erfolgt.
-                  Die Daten werden ausschließlich für mein Forschungsprojekt verwendet.
+                  Es wird sichergestellt, dass eine Zurückverfolgung auf die teilnehmende Person nicht
+                  erfolgt. Die Daten werden ausschließlich für mein Forschungsprojekt verwendet.
             """)
+st.markdown(hinweise_modul)
 
 #Trennungslinie
 st.divider()
 
 #Text zum Start des Schulmoduls
-st.markdown("Bist du Bereit mit der Lerneinheit zu beginnen und über KI zu lernen, dann klicke auf \"Start\" ")
+start_text=("""
+               Bist du Bereit mit der Lerneinheit zu beginnen und über KI zu lernen,
+               dann klicke auf \"Start\"
+            """)
+st.markdown(start_text)
 
 
-#Anpassung der Position des Buttons, CSS funktioniert hier nicht nur durch Hijacking.
-#Anpassung durch Spalten, wobei die erste 80% und die andere 20 % annimmt. 
-#Bei entsprechender Auflösung und Gerät ist der Button rechts. Bei kleineren Seiten funktioniert das nicht
-#Spalten werden bei kleinen Bildschirmen immer untereinander dargestellt.
-
-#2 Spalten erstellen
-col1, col2 = st.columns([8,2])
-
-#In der 2 Spalte also rechts soll der Button plaziert werden
-with col2:
-  
 #Abfrage zur nächsten Seite.
-    if st.button("Start"):
+if st.button("Start"):
 #Beginn der Messung der Startzeit der Umfrage.
-      startzeit = datetime.datetime.now()
+      zeit_start = datetime.datetime.now()
 #Speichern der Startzeit.
-      st.session_state["startzeit"] =startzeit
+      st.session_state["startzeit"] = zeit_start
 #Weiterleitung zur nächsten Seite.
-      st.switch_page("pages/2_Umfrage.py")
+      naechste_seite ="pages/2_Umfrage.py"
+      st.switch_page(naechste_seite)

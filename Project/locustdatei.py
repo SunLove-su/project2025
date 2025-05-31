a6f62598d51fc4e81ed7dfe900a6cc91ca126ca4
sunlove <techDoku89@outlook.de>
2025-05-31 10:51:29 +0200

    testen

--- Project/locustdatei.py
+++ Project/locustdatei.py
@@ -5,12 +5,14 @@ class AppUser(HttpUser):
     wait_time = between(2,5)
 
     
-    def login(self):
+    def on_start(self):
         basis_passwort = os.getenv("UMFRAGE_PASSWORT")
-        self.client.post("/", { "Passwort": "basis_passwort"})
+        self.client.post("/", { "Passwort": basis_passwort})
 
     @task
     def startseite(self):
+        
+
         self.client.get("/")
 
     @task
@@ -26,7 +28,7 @@ class AppUser(HttpUser):
     #Übung_1
     @task
     def uebung1(self):
-        self.client.get("//%C3%9Cbung_1")
+        self.client.get("/%C3%9Cbung_1")
         frage = "Wer ist der atuelle Präsident der USA"
         
         self.client.post("/%C3%9Cbung_1", data = {"frage": frage, "senden": "Fragen"})

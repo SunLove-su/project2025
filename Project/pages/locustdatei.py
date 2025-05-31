07a42d720f50ca887a42c93ca8d4dec4f67b37e3
sunlove <techDoku89@outlook.de>
2025-05-31 09:59:05 +0200

    locusdatei

---
+++ Project/pages/locustdatei.py
@@ -0,0 +1,31 @@
+from locust import HttpUser,task,between 
+
+class AppUser(HttpUser):
+    wait_time = between(2,5)
+
+    @task
+    def login(self):
+        basis_passwort = os.getenv("UMFRAGE_PASSWORT")
+            self.client.post("/", { "Passwort": "basis_passwort"})
+
+    @task
+    def startseite(self):
+        self.client.get("/")
+
+    @task
+    def einstiegsumfrage(self):
+        self.client.get("/Umfrage")
+        data = {
+            "alter": "Unter 15 Jahre",
+            "geschlecht" :"weiblich",
+            "ki_wissen" : "sehr gut"
+            }
+        self.client.post("/Umfrage", data=data)
+
+    #Übung_1
+    @task
+    def uebung1(self):
+        self.client.get("/Übung_1")
+        frage = "Wer ist der atuelle Präsident der USA"
+        
+        self.client.post("/Übung_1", data = {"frage": frage, "senden": "Fragen"})
